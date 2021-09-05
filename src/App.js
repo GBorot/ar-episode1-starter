71d99989375e903b8dc4be46fd76e1df2883411b
Gautier Borot <g.borot@gmail.com>
2021-09-05 23:10:31 +0200

    layout done, pages and router done, auto redimension height from devices, animations of the page done, need to make the animations of the navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,101 @@
-import React, {useEffect} from "react";
-import Banner from "./components/banner";
-import Header from "./components/header";
+import React, {useEffect, useState} from "react";
+import { Route } from 'react-router-dom'
+
+import gsap from "gsap/all";
 import "./styles/App.scss";
 
+// COMPONENTS
+import Header from "./components/header";
+import Navigation from "./components/navigation";
+
+// PAGES
+import Home from "./pages/home";
+import CaseStudies from './pages/caseStudies'
+import Approach from './pages/approach'
+import Services from './pages/services'
+import About from './pages/about'
+
+// ROUTES
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    Component: Home
+  },
+  {
+    path: '/case-studies',
+    name: "Case Studies",
+    Component: CaseStudies
+  },
+  {
+    path: '/approach',
+    name: "Approach",
+    Component: Approach
+  },
+  {
+    path: '/services',
+    name: "Services",
+    Component: Services
+  },
+  {
+    path: '/about-us',
+    name: "About",
+    Component: About
+  }
+]
+
+
+function debounce(fn, ms) {
+  let timer;
+  return () => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      timer = null
+      fn.apply(this, arguments)
+    }, ms);
+  }
+}
+
 function App() {
+  const [dimensions, setDimensions] = useState({
+    heigt: window.innerHeight,
+    width: window.innerWidth
+  })
 
   useEffect(() => {
-    let vh = window.innerHeight * .01;
+    // EMPECHER LE RELOAD BLANC 
+    // PREVENTS FLASHING
+    gsap.to('body', 0, { css: { visibility: "visible" } })
+
+    let vh = dimensions.heigt * .01;
     document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        heigt: window.innerHeight,
+        width: window.innerWidth
+      })
+    }, 1000)
+
+    window.addEventListener('resize', debouncedHandleResize)
+
+    return () => {
+      window.removeEventListener('resize', debouncedHandleResize)
+    }
   }, [])
 
-
   return (
-    <div className='App'>
+    <>
       <Header/>
-      <Banner/>
-    </div>
+      <div className="app">
+        {routes.map(({path, Component}) => (
+          <Route key={path} exact path={path}>
+            <Component/>
+          </Route>
+        ))}
+      </div>
+      <Navigation/>
+    </>
   );
 }
 
--- src/components/banner.js
+++ src/components/banner.js
@@ -3,14 +3,14 @@ import React from 'react';
 // importing svg as a react component
 import { ReactComponent as RightArrow } from '../assets/arrow-right.svg'
 
-const Banner = () => {
+const Banner = ({title}) => {
     return (
         <section className="main">
             <div className="container">
                 <div className="row">
                     <h2>
                         <div className="line">
-                            <span>Creaing unique brand is</span>
+                            <span>Creating unique brands is</span>
                         </div>
                         <div className="line">
                             <span>what we do.</span>
